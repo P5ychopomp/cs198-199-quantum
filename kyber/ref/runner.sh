eb07f6d1e1579d23e5a088f2335c367f88a69f54
P5ychopomp <dsbugaoisan@up.edu.ph>
2024-06-21 17:28:09 +0800

    feat: final

--- kyber/ref/runner.sh
+++ kyber/ref/runner.sh
@@ -5,10 +5,10 @@
 #SBATCH --ntasks-per-node=60
 #SBATCH --output=QUANTUMlog.log
 
-echo "SLURM_JOBID="$SLURM_JOBID
-echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
-echo "SLURM_NNODES"=$SLURM_NNODES
-echo "Working directory = "$SLURM_SUBMIT_DIR
+# echo "SLURM_JOBID="$SLURM_JOBID
+# echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
+# echo "SLURM_NNODES"=$SLURM_NNODES
+# echo "Working directory = "$SLURM_SUBMIT_DIR
 
 run_gcc_script512() {
     ./test_speed512 $1
@@ -20,23 +20,25 @@ run_gcc_script1024() {
     ./test_speed1024 $1
 }
 
-rm -f benchmark_hpc.csv
+filename="benchmark_desktop_ram16gb.csv"
+
+rm -f $filename
 
 echo
 echo "Benchmarking..."
 
-echo "Iteration, , Kyber512, , , Kyber768, , , Kyber1024, , Duration" > benchmark_hpc.csv
-echo " , Keygen, Enc, Dec, Keygen, Enc, Dec, Keygen, Enc, Dec, " >> benchmark_hpc.csv
+echo "Iteration, , Kyber512, , , Kyber768, , , Kyber1024, , Duration" > $filename
+echo " , Keygen, Enc, Dec, Keygen, Enc, Dec, Keygen, Enc, Dec, " >> $filename
 
 # Run the Python script for 30 iterations
 for ((i = 1; i <= 30; i++)); do
     echo "Started Iteration $i."
-    echo -n "$i, " >> benchmark_hpc.csv
+    echo -n "$i, " >> $filename
     # Run the Python script and append the benchmark_hpc to the CSV file
-    run_gcc_script512 $i >> benchmark_hpc.csv
-    run_gcc_script768 $i >> benchmark_hpc.csv
-    run_gcc_script1024 $i >> benchmark_hpc.csv
-    echo "" >> benchmark_hpc.csv
+    run_gcc_script512 $i >> $filename
+    run_gcc_script768 $i >> $filename
+    run_gcc_script1024 $i >> $filename
+    echo "" >> $filename
 done
 
 echo "Done."
